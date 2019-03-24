52ba0684c2087192f271a5a86e705ce88bba6a58
cheyennechan <bbbna_cheyenne@yahoo.com>
2019-03-25 00:30:13 +0800

    ct更新了主页

--- src/components/homePage/index.vue
+++ src/components/homePage/index.vue
@@ -189,7 +189,7 @@
           </router-link>
         </span>
 
-        <div class="swiper-container">
+        <div class="swiper-container swiper-no-swiping">
           <div class="swiper-wrapper">
             <div class="swiper-slide" v-for="item in list">
               <router-link to>{{item}}</router-link>
@@ -203,6 +203,220 @@
         </router-link>
       </div>
     </div>
+    <!-- 热销品类 -->
+    <div class="hotSale">
+      <div class="saleTitle">
+        <p></p>
+        <h2>热销品类</h2>
+        <p></p>
+      </div>
+      <div class="hotBox">
+        <aside>
+          <div class="swiper-container">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide">
+                <router-link to>
+                  <dl>
+                    <dt>
+                      <img src="../../../static/img/pic01.jpg">
+                    </dt>
+                    <dd>
+                      <p>10支纳米牙刷软毛成人家用竹炭抑菌款</p>
+                      <div>
+                        <i>￥</i>
+                        <span>29.00</span>
+                      </div>
+                    </dd>
+                  </dl>
+                </router-link>
+              </div>
+              <div class="swiper-slide">
+                <router-link to>
+                  <dl>
+                    <dt>
+                      <img src="../../../static/img/pic03.jpg">
+                    </dt>
+                    <dd>
+                      <p>新品】康师傅金汤虾球面，营养健康新体验.</p>
+                      <div>
+                        <i>￥</i>
+                        <span>5.00</span>
+                      </div>
+                    </dd>
+                  </dl>
+                </router-link>
+              </div>
+              <div class="swiper-slide">
+                <router-link to>
+                  <dl>
+                    <dt>
+                      <img src="../../../static/img/pic08.jpg">
+                    </dt>
+                    <dd>
+                      <p>RIO锐澳鸡尾酒套装预调酒果酒洋酒经典275ml*24瓶整箱正品1517104正品</p>
+                      <div>
+                        <i>￥</i>
+                        <span>199.00</span>
+                      </div>
+                    </dd>
+                  </dl>
+                </router-link>
+              </div>
+            </div>
+            <!-- 分页器 -->
+            <div class="swiper-pagination"></div>
+          </div>
+        </aside>
+        <article>
+          <div class="rTop">
+            <router-link to="#">
+              <img src="../../../static/img/index_ad_pic(2).jpg">
+            </router-link>
+          </div>
+          <div class="rBotm">
+            <div>
+              <router-link to="#">
+                <img src="../../../static/img/index_ad_pic(3).jpg">
+              </router-link>
+            </div>
+            <div>
+              <router-link to="#">
+                <img src="../../../static/img/index_ad_pic(6).jpg">
+              </router-link>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
+    <!-- 广告条 -->
+    <div class="ads">
+      <div class="swiper-container swiper-no-swiping">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <router-link to>
+              <img src="../../../static/img/ad_pic01.jpg">
+            </router-link>
+          </div>
+          <div class="swiper-slide">
+            <router-link to>
+              <img src="../../../static/img/ad_pic02.jpg">
+            </router-link>
+          </div>
+          <div class="swiper-slide">
+            <router-link to>
+              <img src="../../../static/img/ad_pic03.jpg">
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- 新品推荐 -->
+    <div class="newProducts">
+      <div class="saleTitle">
+        <p></p>
+        <h2>新品推荐</h2>
+        <p></p>
+      </div>
+      <div class="newProdCont">
+        <dl>
+          <router-link to>
+            <dt>
+              <img src="../../../static/img/pic01.jpg">
+            </dt>
+            <dd>
+              <p>10支纳米牙刷软毛成人家用竹炭抑菌款</p>
+              <div>
+                <i>￥</i>
+                <span>29.00</span>
+              </div>
+            </dd>
+          </router-link>
+        </dl>
+        <dl>
+          <router-link to>
+            <dt>
+              <img src="../../../static/img/pic.jpg">
+            </dt>
+            <dd>
+              <p>日本进口食品丸京红豆铜锣烧310g蛋糕面包早餐网红零食品小吃动网球鞋</p>
+              <div>
+                <i>￥</i>
+                <span>299.00</span>
+              </div>
+            </dd>
+          </router-link>
+        </dl>
+        <dl>
+          <router-link to>
+            <dt>
+              <img src="../../../static/img/pic03.jpg">
+            </dt>
+            <dd>
+              <p>康师傅红烧牛肉面康师傅红烧牛肉面康师傅红烧牛肉面</p>
+              <div>
+                <i>￥</i>
+                <span>49.00</span>
+              </div>
+            </dd>
+          </router-link>
+        </dl>
+        <dl>
+          <router-link to>
+            <dt>
+              <img src="../../../static/img/pic08.jpg">
+            </dt>
+            <dd>
+              <p>RIO锐澳鸡尾酒套装预调酒果酒洋酒经典275ml*24瓶整箱正品1517104正品</p>
+              <div>
+                <i>￥</i>
+                <span>399.00</span>
+              </div>
+            </dd>
+          </router-link>
+        </dl>
+        <dl>
+          <router-link to>
+            <dt>
+              <img src="../../../static/img/pic05.jpg">
+            </dt>
+            <dd>
+              <p>innisfree/悦诗风吟绿茶精萃保湿洁面乳</p>
+              <div>
+                <i>￥</i>
+                <span>299.00</span>
+              </div>
+            </dd>
+          </router-link>
+        </dl>
+        <dl>
+          <router-link to>
+            <dt>
+              <img src="../../../static/img/pic06.jpg">
+            </dt>
+            <dd>
+              <p>化妆品收纳盒桌面抽屉式办公室文具文件架办公桌用品多功能置物架</p>
+              <div>
+                <i>￥</i>
+                <span>199.00</span>
+              </div>
+            </dd>
+          </router-link>
+        </dl>
+      </div>
+      <div class="moreLink">
+        <router-link to="">更多新品></router-link>
+      </div>
+    </div>
+    <div class="loadMore">
+      加载更多···
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
   </div>
 </template>
 
@@ -254,7 +468,7 @@
 }
 // 商城公告商家入驻
 .newsMerchant {
-  border-bottom: .38rem solid #f1f1f1;
+  border-bottom: 0.38rem solid #f1f1f1;
   .roll {
     height: 1.25rem;
     padding: 2%;
@@ -289,6 +503,154 @@
     }
   }
 }
+.saleTitle {
+  width: 56%;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: space-between;
+  line-height: 1.88rem;
+  display: flex;
+  padding: 3% 0 5%;
+  p {
+    width: 2.7rem;
+    height: 1px;
+    background-color: #4aa3e6;
+  }
+  h2 {
+    font-weight: 100;
+    color: #4aa3e6;
+    font-size: 1.13rem;
+    background: url(../../../static/img/line_botm.jpg) no-repeat center bottom;
+    background-size: 100% 3px;
+  }
+}
+// 热销品类
+.hotSale {
+  border-bottom: 6px solid #f1f1f1;
+  padding-bottom: 5%;
+
+  .hotBox {
+    border-top: #ddd solid 1px;
+    border-bottom: #ddd solid 1px;
+    display: flex;
+    aside,
+    article {
+      width: 50%;
+      img {
+        width: 100%;
+        display: block;
+      }
+    }
+    aside {
+      border-right: #ddd solid 1px;
+      .swiper-container {
+        margin: 4%;
+        padding-bottom: 10%;
+      }
+      p {
+        margin-top: 3%;
+        color: #666;
+        font-size: 0.69rem;
+        line-height: 0.88rem;
+      }
+      div {
+        color: #f00;
+        font-size: 0.75rem;
+      }
+    }
+    article {
+      .rTop {
+        padding: 2%;
+        height: 6.56rem;
+        border-bottom: #ddd solid 1px;
+      }
+      .rBotm {
+        display: flex;
+        div {
+          width: 48%;
+          padding: 3% 1%;
+        }
+        div:nth-of-type(1) {
+          border-right: #ddd solid 1px;
+        }
+      }
+    }
+    .swiper-pagination-bullet-active {
+      background: #007aff !important;
+    }
+    .swiper-pagination-bullet {
+      width: 12px;
+      height: 3px;
+      border-radius: 0;
+      margin: 0 2px;
+      background-color: #ffe037;
+      opacity: 1;
+    }
+  }
+}
+// 广告条
+.ads {
+  border-bottom: 6px solid #f1f1f1;
+  img {
+    width: 100%;
+    display: block;
+  }
+}
+// 新品推荐
+.newProducts{
+  .newProdCont {
+    display: flex;
+    flex-wrap: wrap;
+    dl {
+      width: 31.7333%;
+      margin: 0 0.8%;
+      margin-bottom: 1%;
+      dd{
+        p{
+          color: #666;
+          font-size: .69rem;
+          line-height: .88rem;
+          height: 1.68rem;
+          margin-top: 4%;
+          overflow: hidden;
+        }
+        div{
+          color: #f00;
+          font-size: .81rem;
+          line-height: 160%;
+          margin: 0 2% 4%;
+        }
+      }
+      img {
+        width: 100%;
+      }
+    }
+  }
+  .moreLink{
+    padding: 3% 0 7%;
+    a{
+      display: block;
+      width: 50%;
+      background-color: #2691e1;
+      border-radius: 50px;
+      text-align: center;
+      margin: 0 auto;
+      height: 1.88rem;
+      line-height: 1.88rem;
+      font-size: .81rem;
+      color: #fff;
+    }
+    
+  }
+}
+.loadMore{
+  background-color: #f0f0f0;
+  text-align: center;
+  color: #999;
+  font-size: .69rem;
+  padding: 2%
+}
+
 </style>
 
 <script>
@@ -326,6 +688,25 @@ export default {
     var newsSwiper = new Swiper(".newsMerchant .swiper-container", {
       direction: "vertical", // 垂直切换选项
       loop: true, // 循环模式选项
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      }
+    });
+    var hotSaleSwiper = new Swiper(".hotSale .swiper-container", {
+      loop: true, // 循环模式
+      // 分页器
+      pagination: {
+        el: ".swiper-pagination"
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+      }
+    });
+    var adsSwiper = new Swiper(".ads .swiper-container", {
+      loop: true, // 循环模式
+      effect: "fade",
       autoplay: {
         delay: 3000,
         disableOnInteraction: false
